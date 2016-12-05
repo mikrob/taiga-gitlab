3c1b3314b29df28e5aa8164fb121e49ef3457af9
Benjamin Jorand <benjamin.jorand@botsunit.com>
2016-12-05 18:44:00 +0100

    import script

---
+++ import.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+export GITLAB_TOKEN=1qVsgb99XFst2GRwBXxn
+export TAIGA_PASSWORD=botsunit8075
+
+for i in $(cat repo.list); do
+  go run run.go import --taiga-url https://taiga.botsunit.io --taiga-user admin --taiga-project Ufancyme --gitlab-url https://gitlab.botsunit.com --gitlab-project $i
+done
