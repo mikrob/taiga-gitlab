cf73ae5be9fed28081ba18ddf7a13d877bb8d684
Benjamin Jorand <benjamin.jorand@botsunit.com>
2016-12-07 11:12:07 +0100

    create tasks during import

--- import.sh
+++ import.sh
@@ -4,6 +4,6 @@ export GITLAB_TOKEN=1qVsgb99XFst2GRwBXxn
 export TAIGA_PASSWORD=botsunit8075
 
 for i in $(cat repo.list); do
-  go run run.go import --taiga-url https://taiga.botsunit.io --taiga-user admin --taiga-project Ufancyme --gitlab-url https://gitlab.botsunit.com --gitlab-project $i
+  go run run.go import --taiga-url https://taiga.botsunit.io --taiga-user admin --taiga-project Ufancyme --gitlab-url https://gitlab.botsunit.com --gitlab-project $i --create-task
   [[ $? -gt 0 ]] && exit 1
 done
