6a4c3ea777c560dccfaff857d63f0d8bb453b6a0
mikrob <mikael.robert@botsunit.com>
2017-05-11 11:30:47 +0200

    uniformize

--- import-dev.sh
+++ import-dev.sh
@@ -4,6 +4,6 @@ export GITLAB_TOKEN=1qVsgb99XFst2GRwBXxn
 export TAIGA_PASSWORD=123123
 
 for i in $(cat repo.list); do
-  go run run.go import --taiga-url http://dev:81 --taiga-user admin --taiga-project ufancyme --gitlab-url https://gitlab.botsunit.com --gitlab-project $i --taiga-skip-user --create-task
+  go run run.go import --taiga-url http://dev:81 --taiga-user admin --taiga-project project --gitlab-url https://gitlab.project.com --gitlab-project $i --taiga-skip-user --create-task
   [[ $? -gt 0 ]] && exit 1
 done
--- import.sh
+++ import.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-export GITLAB_TOKEN=1qVsgb99XFst2GRwBXxn
-export TAIGA_PASSWORD=botsunit8075
+export GITLAB_TOKEN=GITLABTOKEN
+export TAIGA_PASSWORD=TAIGAPASSWORD
 
 for i in $(cat repo.list); do
-  go run run.go import --taiga-url https://taiga.botsunit.io --taiga-user admin --taiga-project Ufancyme --gitlab-url https://gitlab.botsunit.com --gitlab-project $i --create-task
+  go run run.go import --taiga-url https://taiga.project.io --taiga-user admin --taiga-project project --gitlab-url https://gitlab.project.com --gitlab-project $i --create-task
   [[ $? -gt 0 ]] && exit 1
 done
